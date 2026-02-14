230d6a2419e249ea6f33d9ec7d8d2d4f34110073
Kartikeya Pusapati <swagboss600@gmail.com>
2026-02-14 10:51:12 +0530

    [Kartikeya]: UC1 refactor - updated class comment and static message

--- FactoryRobotHazardAnalyzer.java
+++ FactoryRobotHazardAnalyzer.java
@@ -1,3 +1,13 @@
+/**
+ * FactoryRobotHazardAnalyzer
+ *
+ * This class serves as the entry point for the Factory Robot Hazard Analyzer application.
+ * The system incrementally evolves across multiple use cases (UC1, UC2, etc.)
+ * to analyze hazards associated with industrial robotic environments.
+ *
+ * UC1: Displays a static system message.
+*Author:Kartikeya
+ */
 public class FactoryRobotHazardAnalyzer {
     public static void main(String[] args) {
         System.out.println("Factory Robot Hazard Analyzer");
